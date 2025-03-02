f2fb11b348ee8a436feb8a475fc996caaafbc288
shahidulhasansarker <shahidulhasansarker@gmail.com.com>
2025-03-02 16:42:14 +0600

    task

---
+++ javascript/taskCompleted.js
@@ -0,0 +1,14 @@
+const taskAssign = document.querySelectorAll(".completedBtn");
+for(let comBtn of taskAssign){
+   comBtn.addEventListener('click',function(){
+    const amount = event.target.innerText === "Completed" ? -1 : 1;
+   
+    const quantityElement = document.getElementById('quantity');
+    const currentQuentity = parseInt(quantityElement.innerText);
+    
+    const newQuantity = Math.max(0, currentQuentity + amount);
+    
+    quantityElement.innerText = newQuantity;
+    alert('Thanks for Completing this Task');
+   })
+}
